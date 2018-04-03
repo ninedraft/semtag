dec85e283595f2a1558928c04c12d69d0f159ce1
paul <petrukhin.paul@gmail.com>
2018-04-03 10:36:07 +0300

    add all flag

--- get.go
+++ get.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/ninedraft/semtag/git"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var getCurrentVersionAliases = []string{}
+var getCurrentVersionAliases = []string{"tag", "tags"}
 var getCurrentVersionConfig struct {
+	All bool
 }
 var getCurrentVersion = &cobra.Command{
-	Use:     "get",
+	Use:     "show",
 	Aliases: getCurrentVersionAliases,
 	PreRun: func(command *cobra.Command, args []string) {
 
 	},
 	Run: func(command *cobra.Command, args []string) {
+		if getCurrentVersionConfig.All {
+			repo := git.Repo{}
+			tags, err := repo.Tags()
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(strings.Join(tags, "\n"))
+			return
+		}
 		version, err := getHeadTag("")
 		switch {
 		case err == nil:
@@ -26,4 +39,9 @@ var getCurrentVersion = &cobra.Command{
 		}
 		fmt.Printf("v%v\n", version)
 	},
+}
+
+func init() {
+	getCurrentVersion.PersistentFlags().BoolVarP(&getCurrentVersionConfig.All, "all", "a", false, "show all versions")
+	viper.BindPFlag("all", increment.PersistentFlags().Lookup("all"))
 }
